97d2476a47fd2046ccd4957169c9039351b6cb66
Stefan Berglas <st.berglas@bluewin.ch>
2025-11-04 11:28:26 +0100

    app.axaml and mainviewmodel modified

--- CryptoWorkBenchAvalonia/Views/MainWindow.axaml.cs
+++ CryptoWorkBenchAvalonia/Views/MainWindow.axaml.cs
@@ -1,11 +1,14 @@
 ﻿using Avalonia.Controls;
+using CryptoWorkBenchAvalonia.ViewModels;
 
 namespace CryptoWorkBenchAvalonia.Views;
 
 public partial class MainWindow : Window
 {
-    public MainWindow()
+    
+    public MainWindow(MainViewModel vm)
     {
         InitializeComponent();
+        DataContext = vm;
     }
 }
